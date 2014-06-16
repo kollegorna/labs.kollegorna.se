7e7d52077af6110d05612aa697ebd9de88ea96f2
Per Sandström <per@helloper.com>
2014-06-16 12:01:10 +0200

    Add more projects

--- app/_assets/javascripts/app/main.js
+++ app/_assets/javascripts/app/main.js
@@ -1,3 +1,6 @@
 $(function() {
-
+  // Set body min height to window height to make footer border looks nice.
+  if (Modernizr.mq('(min-width: 1024px)')) {
+    $('body').css('min-height', $(window).height());
+  }
 });
