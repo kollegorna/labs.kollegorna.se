5229cdf0bcad4f45714a9ac22c12bf6f79e98b66
Per Sandström <per@helloper.com>
2015-03-31 14:02:35 +0200

    Moar layout stuff

--- app/_assets/javascripts/app/main.js
+++ app/_assets/javascripts/app/main.js
@@ -1,11 +1,8 @@
 $(function() {
-  // Set body min height to window height to make footer border looks nice.
-  if (Modernizr.mq('(min-width: 1024px)')) {
-    $('body').css('min-height', $(window).height());
-  }
-
   $( ".index__about-toggle" ).click(function(e) {
-    e.preventDefault();
-    $( ".index__about, .index__blog, .index__projects" ).fadeToggle( "fast" );
+    if (Modernizr.mq('(min-width: 1024px)')) {
+      e.preventDefault();
+      $( ".index__about, .index__blog, .index__projects" ).fadeToggle( "fast" );
+    }
   });
 });
