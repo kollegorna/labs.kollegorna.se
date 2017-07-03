d07632ca525b6fe8dafa6f2b28516e7f8e9e8e69
Osvaldas Valutis <osvaldas.v@gmail.com>
2017-07-03 14:23:29 +0300

    Permalinks for Disqus fix: wip

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -2,8 +2,19 @@
 //= require "gist-embed"
 //= require "disqusLoader.js/jquery.disqusloader.js"
 
-;( function( $ )
-{
-    $.disqusLoader( '.disqus', { scriptUrl: '//kollegornalabs.disqus.com/embed.js' });
 
-})( jQuery );
+// lazy load disqus comments
+
+;( function() {
+
+  var url = window.location.origin + window.location.pathname;
+  console.log(url);
+  $.disqusLoader( '.disqus', {
+    scriptUrl: '//kollegornalabs.disqus.com/embed.js',
+    disqusConfig: function() {
+      this.page.url         = url;
+      this.page.identifier  = url;
+    }
+  });
+
+})();
